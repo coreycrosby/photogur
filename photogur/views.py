7958a887d3e40c641c8ada6d012de81a3684dfca
Corey Crosby <ccc_13@live.ca>
2019-05-24 15:38:45 -0400

    completed exercise

---
+++ photogur/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from photogur.models import Picture
+
+
+def pictures(request):
+    context = {'pictures': Picture.objects.all()}
+    response = render(request, 'pictures.html', context)
+    return HttpResponse(response)
+
+
+def root(request):
+    return HttpResponseRedirect('admin')
